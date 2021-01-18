220b160622a91c0d8d3c88985e8535e18a8a8bca
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-01-18 16:52:05 -0600

    Clonando el repositorio e instalando Webpack

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  output: {
+    filename: 'app.bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin()
+  ]
+};
