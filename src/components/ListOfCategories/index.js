38b41dfed398da34dbe05490f75b8b03fa8fcfc5
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-01-19 16:41:29 -0600

    Implementando Hooks

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Category } from '../Category'
 import { List, Item } from './styles'
-import { categories } from '../../../api/db.json'
 
 export const ListOfCategories = () => {
+  const [categories, setCategories] = useState([])
+
+  useEffect(function () {
+    window
+      .fetch('https://petgram-server-two.vercel.app/categories')
+      .then(res => res.json()) /* Converimos la respuesra Json */
+      .then(response => {
+        setCategories(response)
+      })
+  }, [])
+
   return (
     <List>
       {categories.map(category => (
