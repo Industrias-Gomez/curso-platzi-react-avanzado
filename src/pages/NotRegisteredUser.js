372489eea6365fc72a8e625889f814a478a38c6b
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-01-21 15:32:26 -0600

    Persistiendo datos en Session Storage

--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -1,59 +1,62 @@
-import React from 'react'
-import Context from '../Context'
+import React, { useContext } from 'react'
+import { Context } from '../Context'
 import { UserForm } from '../components/UserForm'
 import { RegisterMutation } from '../container/RegisterMutation'
 import { LoginMutation } from '../container/LoginMutation'
 
-export const NotRegisteredUser = () => (
-  <Context.Consumer>
-    {({ activateAuth }) => {
-      return (
-        <>
-          <RegisterMutation>
-            {(register, { data, loading, error }) => {
-              const onSubmit = ({ email, password }) => {
-                const input = { email, password }
-                const variables = { input }
-                register({ variables }).then(activateAuth) //Validamos el resultado de la promesa con el resultado validamos que se registro el usuario
-              }
+export const NotRegisteredUser = () => {
+  const { activateAuth } = useContext(Context)
 
-              const errorMsg =
-                error && 'El usuario ya existe o hay algún problema.'
+  return (
+    <>
+      <RegisterMutation>
+        {(register, { data, loading, error }) => {
+          const onSubmit = ({ email, password }) => {
+            const input = { email, password }
+            const variables = { input }
+            register({ variables }).then(({ data }) => {
+              const { signup } = data
+              activateAuth(signup)
+            }) //Validamos el resultado de la promesa con el resultado validamos que se registro el usuario
+          }
 
-              return (
-                <UserForm
-                  disabled={loading}
-                  error={errorMsg}
-                  title='Registrarse'
-                  onSubmit={onSubmit}
-                />
-              )
-            }}
-          </RegisterMutation>
+          const errorMsg = error && 'El usuario ya existe o hay algún problema.'
 
-          <LoginMutation>
-            {(login, { data, loading, error }) => {
-              const onSubmit = ({ email, password }) => {
-                const input = { email, password }
-                const variables = { input }
-                login({ variables }).then(activateAuth) //Validamos el resultado de la promesa con el resultado validamos que se registro el usuario
-              }
+          return (
+            <UserForm
+              disabled={loading}
+              error={errorMsg}
+              title='Registrarse'
+              onSubmit={onSubmit}
+            />
+          )
+        }}
+      </RegisterMutation>
 
-              const errorMsg =
-                error && 'La contraseña no es correcta o el usuario ya existe.'
+      <LoginMutation>
+        {(login, { data, loading, error }) => {
+          const onSubmit = ({ email, password }) => {
+            const input = { email, password }
+            const variables = { input }
+            login({ variables }).then(({ data }) => {
+              const { login } = data
+              activateAuth(login)
+            }) //Validamos el resultado de la promesa con el resultado validamos que se registro el usuario
+          }
 
-              return (
-                <UserForm
-                  disabled={loading}
-                  error={errorMsg}
-                  title='Iniciar sesión'
-                  onSubmit={onSubmit}
-                />
-              )
-            }}
-          </LoginMutation>
-        </>
-      )
-    }}
-  </Context.Consumer>
-)
+          const errorMsg =
+            error && 'La contraseña no es correcta o el usuario ya existe.'
+
+          return (
+            <UserForm
+              disabled={loading}
+              error={errorMsg}
+              title='Iniciar sesión'
+              onSubmit={onSubmit}
+            />
+          )
+        }}
+      </LoginMutation>
+    </>
+  )
+}
