4b36e26c60c786cc3f940237f6e140349f948e29
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-01-21 17:24:24 -0600

    Últimos retoques a las rutas de nuestra aplicación

--- src/pages/User.js
+++ src/pages/User.js
@@ -2,7 +2,6 @@ import React, { useContext } from 'react'
 import { Context } from '../Context'
 import { SubmitButton } from '../components/SubmitButton'
 
-
 export const User = () => {
   const { removeAuth } = useContext(Context)
   return (
