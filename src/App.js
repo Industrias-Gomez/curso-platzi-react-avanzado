c9125b4c7eb855676452116469f4111393b0ee4d
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-01-22 11:29:53 -0600

    React.lazy() y componente Suspense

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import React, { useContext } from 'react'
+import React, { useContext, Suspense } from 'react'
 import { GlobalStyles } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
 import { NavBar } from './components/NavBar'
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
-import { Favs } from './pages/Favs'
+// import { Favs } from './pages/Favs'
 import { User } from './pages/User'
 import { NotRegisteredUser } from './pages/NotRegisteredUser'
 import { NotFound } from './pages/NotFound'
 import { Redirect, Router } from '@reach/router'
 import { Context } from './Context'
 
+// React.lazy nos va a permitir suspender algo cuando está en modo lazy
+const Favs = React.lazy(() => import('./pages/Favs'))
+
 export const App = () => {
   const { isAuth } = useContext(Context)
   return (
-    <>
+    <Suspense fallback={<div />}>
       <GlobalStyles />
       <Logo />
       <Router>
@@ -31,6 +34,6 @@ export const App = () => {
         <User path='/user' />
       </Router>
       <NavBar />
-    </>
+    </Suspense>
   )
 }
