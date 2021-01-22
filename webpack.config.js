a6edec388401b0817091c8416b7957322d5ec670
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-01-22 12:56:01 -0600

    PWA: soporte offline

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const WebpackPwaManifestPlugin = require('webpack-pwa-manifest')
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin')
 const path = require('path')
 
 module.exports = {
@@ -14,13 +15,32 @@ module.exports = {
     new WebpackPwaManifestPlugin({
       name: 'Petgram - Tu app de fotos de mascotas',
       short_name: 'Petgram 🐶',
-      description: 'Con Petgram puedes encontrar fotos de animales domésticos muy fácilmente',
+      description:
+        'Con Petgram puedes encontrar fotos de animales domésticos muy fácilmente',
       background_color: '#fff',
       theme_color: '#b1a',
       icons: [
         {
           src: path.resolve('src/assets/icon.png'),
           size: [96, 128, 192, 256, 384, 512]
+        }
+      ]
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp('https://(res.cloudinary.com|images.unsplash.com)'),
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'images'
+          }
+        },
+        {
+          urlPattern: new RegExp('https://petgram-server.ericgomez.vercel.app'),
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'api'
+          }
         }
       ]
     })
